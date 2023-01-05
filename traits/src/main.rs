79b152e8e347c9dc14bb061260a587aa48fac0df
Bhavana Chivukula <bhavana.chivukula@everest.engineering>
2023-01-05 13:41:33 +0530

    Default implementation trait

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,9 +1,11 @@
-// Traits
-
 use std::fmt::format;
 
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize(&self) -> String {
+        String::from("Default implementation..")
+    }
+
+    fn summarize_author(&self) -> String;
 }
 
 pub struct Person {
@@ -13,10 +15,10 @@ pub struct Person {
 }
 
 impl Summary for Person {
-    fn summarize(&self) -> String {
-        format!("Name of the person is:{} and age is:{}",self.name,self.age)
+    fn summarize_author(&self) -> String {
+        format!("@ author{}",self.name)
     }
-}
+} 
 
 pub struct Student {
     pub roll_number: u32,
@@ -24,10 +26,13 @@ pub struct Student {
     pub class: u32,
     pub marks_sheet: u32
 }
-
 impl Summary for Student {
     fn summarize(&self) -> String {
         format!("Student name is:{} and roll number is:{}",self.name,self.roll_number)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@ author {}",self.name)
     }
 }
 
@@ -37,7 +42,6 @@ fn main() {
         age:23,
         gender: 'F',
     };
-
     let stu = Student {
         roll_number: 66,
         name: String::from("Bhavana"),
@@ -45,6 +49,6 @@ fn main() {
         marks_sheet: 95,
     };
 
-    println!("Person details = {}",per.summarize());
-    println!("Student details = {}",stu.summarize());
+    println!("Person details: {} {}",per.summarize_author(),per.summarize()); // Person details = Default implementation..
+    println!("Student details: {} {}",per.summarize_author(),stu.summarize()); // Student details = Student name is:Bhavana and roll number is:66
 }
