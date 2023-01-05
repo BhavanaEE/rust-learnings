ef111ad32904832fcf5e8c915e520e37729ba9e6
Bhavana Chivukula <bhavana.chivukula@everest.engineering>
2023-01-05 13:33:38 +0530

    generic function arguments

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -3,18 +3,18 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = get_largest(&number_list);
     println!("The largest number is {}", result);
     assert_eq!(*result, 100);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = get_largest(&char_list);
     println!("The largest char is {}", result);
     assert_eq!(*result, 'y');
 }
 
-fn largest_i32(list: &[i32]) -> &i32 {
+fn get_largest<T: PartialOrd + Copy>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
@@ -22,18 +22,5 @@ fn largest_i32(list: &[i32]) -> &i32 {
             largest = item;
         }
     }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> &char {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
     largest
 }
