10429a65672a30676199dc6e235572f9bca3cb7b
Bhavana Chivukula <bhavana.chivukula@everest.engineering>
2023-01-05 11:31:31 +0530

    updating hashmap word count example

--- hashmap/src/main.rs
+++ hashmap/src/main.rs
@@ -15,4 +15,16 @@ fn main() {
     scores.entry(String::from("yellow")).or_insert(68);
 
     println!("{:?}",scores.get("yellow")); // Some(20)
+
+     // Word count
+     let text = "hello world beautiful world";
+
+     let mut map = HashMap::new();
+ 
+     for word in text.split_whitespace() {
+         let count = map.entry(word).or_insert(0);
+         *count += 1;
+     }
+ 
+     println!("{:?}",map);
 }
