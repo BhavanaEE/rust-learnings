2f6f171afbb93b4f0a56a0786b90dcd377847fe8
Bhavana Chivukula <bhavana.chivukula@everest.engineering>
2023-01-05 11:30:49 +0530

    updating hashmap

--- hashmap/src/main.rs
+++ hashmap/src/main.rs
@@ -1,21 +1,18 @@
 // hashmap collection
 
+//updating hashmap
 use std::collections::HashMap;
 
 fn main() {
-    let blue_team = String::from("blue");
-    let green_team = String::from("green");
-
     let mut scores = HashMap::new();
-    scores.insert(blue_team, 10);
-    scores.insert(green_team, 30);
 
-    // println!("{}",blue_team); //cannot borrow
+    scores.insert(String::from("blue"), 10);
+    scores.insert(String::from("blue"), 30);
 
-    let team_name = String::from("blue");
-    let score = scores.get(&team_name);
+    println!("{:?}",scores.get("blue")); // Some(30) - overrides
 
-    for (key,value) in scores {
-        println!("{}{}",key,value);
-    }
+    scores.entry(String::from("yellow")).or_insert(20);
+    scores.entry(String::from("yellow")).or_insert(68);
+
+    println!("{:?}",scores.get("yellow")); // Some(20)
 }
