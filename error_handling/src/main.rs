3acd491c1d87f599328ea78cf582d613c426d0d3
Bhavana Chivukula <bhavana.chivukula@everest.engineering>
2023-01-05 11:39:50 +0530

    Handling error using nested match expression

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,13 +1,24 @@
 // Error handling
 
-use std::fs::File;
-
+use std::{fs::File, error::Error};
+use std::io::ErrorKind;
 fn main() {
     let mut f = File::open("hello.txt");
 
     // shadowing to redeclare f
     let f = match f {
         Ok(file) => file,
-        Err(error) => panic!("problem while opening a file {:?}",error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file {:?}",e),
+            }
+
+            other_error => {
+                panic!("problem opening the file {:?}",other_error)
+            }
+        }
     };
+
+
 }
