89f834a982d6805314a1b6583d6e44207dd679a8
Bhavana Chivukula <bhavana.chivukula@everest.engineering>
2023-01-05 12:33:28 +0530

    Error propagation

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,30 +1,23 @@
-// Error handling
-
-use std::{fs::File, error::Error};
-use std::io::ErrorKind;
+#![allow(unused)]
 fn main() {
-    let mut f = File::open("hello.txt");
+    use std::fs::File;
+    use std::io::{self, Read};
 
-    // shadowing to redeclare f
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file {:?}",e),
-            }
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let username_file_result = File::open("hello.txt");
 
-            other_error => {
-                panic!("problem opening the file {:?}",other_error)
-            }
+        let mut username_file = match username_file_result {
+            Ok(file) => file,
+            Err(e) => return Err(e),
+        };
+
+        let mut username = String::new();
+
+        match username_file.read_to_string(&mut username) {
+            Ok(_) => Ok(username),
+            Err(e) => Err(e),
         }
-    };
+    }
 
-    //unwrap
-    let f1 = File::open("hello.txt").unwrap();
-
-    // expect
-    let f2 = File::open("hello.txt")
-        .expect("No file named hello.txt");
-
+    println!("{:?}",read_username_from_file()) // Ok("Hey Bhavana")
 }
